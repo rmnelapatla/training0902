0d8023db5eed81c76b5b667ae28a09ebd968f967
Ramana Murthy <rama_70@yahoo.com>
2025-02-09 07:56:57 +0530

    add new line with int

--- src/PK1/Test123.java
+++ src/PK1/Test123.java
@@ -5,5 +5,6 @@ public class Test123 {
     public static void main(String arg[]){
         System.out.println("this first line code for git");
         System.out.println("next line");
+        int b =10;
     }
 }
