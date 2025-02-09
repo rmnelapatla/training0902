92301a20c0c68dcc3bc6fcaeadaedda826734699
Ramana Murthy <rama_70@yahoo.com>
2025-02-09 08:26:06 +0530

    update main file

--- src/PK1/Test123.java
+++ src/PK1/Test123.java
@@ -3,9 +3,6 @@ package PK1;
 public class Test123 {
 
     public static void main(String arg[]){
-        int a = 12;
-        System.out.println("this first line code for git this is new very ");
-        System.out.println("git is very usefull");
-        int b =10;
+        System.out.println("delete ");
     }
 }
