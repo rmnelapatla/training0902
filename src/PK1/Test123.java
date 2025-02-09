5e9ffdb03a749976cb3ddc6da71c8fafa055f882
ramana <rmnelapatla@users.noreply.github.com>
2025-02-09 07:48:15 +0530

    Update Test123.java
    new changes happend

--- src/PK1/Test123.java
+++ src/PK1/Test123.java
@@ -3,7 +3,8 @@ package PK1;
 public class Test123 {
 
     public static void main(String arg[]){
-        System.out.println("this first line code for git");
-        System.out.println("next line");
+        int a = 12;
+        System.out.println("this first line code for git this is new very ");
+        System.out.println("git is very usefull");
     }
 }
