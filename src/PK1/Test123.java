a45e1e38ffcabca876547ac7ef44bba631c62e0a
Ramana Murthy <rama_70@yahoo.com>
2025-02-09 08:21:24 +0530

    add for loop

--- src/PK1/Test123.java
+++ src/PK1/Test123.java
@@ -3,9 +3,13 @@ package PK1;
 public class Test123 {
 
     public static void main(String arg[]){
-        int a = 12;
+        int a1 = 12;
         System.out.println("this first line code for git this is new very ");
         System.out.println("git is very usefull");
         int b =10;
+
+        for(int a =0; a<10;a++){
+            System.out.println(a);
+        }
     }
 }
