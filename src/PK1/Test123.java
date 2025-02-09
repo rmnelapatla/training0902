a76e3a59aa02640dc7e71fd62e76445d1c98cdc8
Ramana Murthy <rama_70@yahoo.com>
2025-02-09 08:02:58 +0530

    change done

--- src/PK1/Test123.java
+++ src/PK1/Test123.java
@@ -6,8 +6,6 @@ public class Test123 {
         int a = 12;
         System.out.println("this first line code for git this is new very ");
         System.out.println("git is very usefull");
-        System.out.println("this first line code for git");
-        System.out.println("next line");
         int b =10;
     }
 }
